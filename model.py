994a853dfb55136fa394b4f4a47738feb0e1bc01
Matt Widjaja <matthew@wimagine.org>
2017-03-25 09:22:30 -0400

    Added K-Means
    Added K-Means clustering to model.py -- originally from PhillyCrime
    project

--- model.py
+++ model.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar  4 10:06:00 2017
-
-@author: Matthew
+Helper Functions to model using Scikit-Learn
 """
 
 import matplotlib.pyplot as plt
-from sklearn import ensemble, linear_model
+from sklearn import cluster, linear_model
 
 
-def features(data_feature, data_target):
-    """ Depricated? Does Feature Importance but plot function in load.py
-        seems better """
-    features = {}
-    model = ensemble.RandomForestRegressor(n_estimators=3)
-    model.fit(data_feature, data_target)
-    for idx, column in enumerate(data_feature.columns):
-        features[column] = model.feature_importances_[idx]
-    return features
+def kmeans(train_data, clusters=8):
+    """ Performs a K-Means clustering algorithm on data.
+
+        Inputs:
+        train_data: A series or list of tuples of (x, y) points
+        clusters: How many clusters to create [Default = 8]
+
+        Outputs:
+        cluster_data: A dict where each cluster's center is a key and its
+                      value is a list of all points connected to that center.
+    """
+    # Performs K-Means Clustering
+    kmeans = cluster.KMeans(n_clusters=clusters,
+                            precompute_distances=True,
+                            n_jobs=-2)
+    if isinstance(train_data, list):
+        kmean_data = kmeans.fit(train_data)
+    else:
+        kmean_data = kmeans.fit(train_data.tolist())
+
+    # Identifies centers for each cluster
+    centers = [(x[0], x[1]) for x in kmean_data.cluster_centers_]
+    center_per_pt = kmean_data.labels_.tolist()
+
+    # Identifies which center each data point is connected to
+    cluster_data = {center: 0 for center in centers}
+    for idx, centers in enumerate(centers):
+        cluster_data[centers] = center_per_pt.count(idx)
+    return cluster_data
 
 
 def linear_regression(train_feature, train_target, test_feature, test_target):
